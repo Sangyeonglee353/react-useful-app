e86cb5b263ea04c13e7d41d5cee62b04e98d61a9
Sang Yeong Lee <Sangyeong.lee353@gmail.com>
2023-09-26 08:15:43 +0900

    Feat: add Navbar and page routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import TransferPage from "./pages/TransferPage";
+import Navbar from "./components/Navbar";
+import { Outlet, Route, Routes } from "react-router-dom";
 
-function App() {
+const Layout = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+
+      <Outlet />
     </div>
   );
-}
+};
+
+const App = () => {
+  return (
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="about" element={<AboutPage />} />
+          <Route path="transfer" element={<TransferPage />} />
+        </Route>
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import { FaAlignJustify, FaTimes } from "react-icons/fa";
+import { Link } from "react-router-dom";
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  return (
+    <nav className="w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg">
+      <div className="flex items-center justify-between w-full h-full px-10">
+        <div className="flex items-center">
+          <h1 className="mr-4 text-3xl font-bold sm:text-4xl">Useful</h1>
+          <ul className="hidden md:flex">
+            <li>
+              <Link to="/" className="cursor-pointer">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="about" className="cursor-pointer">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link to="transfer" className="cursor-pointer">
+                Transfer
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="hidden pr-4 md:flex">
+          <button className="mr-4 text-black bg-transparent border-none">
+            Sign In
+          </button>
+          <button className="px-8 py-3">Sign Up</button>
+        </div>
+        <div className="mr-4 md:hidden" onClick={() => setNav(!nav)}>
+          {!nav ? (
+            <FaAlignJustify className="w-5" />
+          ) : (
+            <FaTimes className="w-5" />
+          )}
+        </div>
+      </div>
+      <ul className={!nav ? "hidden" : "absolute bg-zinc-200 w-full px-8"}>
+        <li className="w-full border-b-2 border-zinc-300">
+          <Link onClick={() => setNav(false)} to="/" className="cursor-pointer">
+            Home
+          </Link>
+        </li>
+        <li className="w-full border-b-2 border-zinc-300">
+          <Link
+            onClick={() => setNav(false)}
+            to="about"
+            className="cursor-pointer"
+          >
+            About
+          </Link>
+        </li>
+        <li className="w-full border-b-2 border-zinc-300">
+          <Link
+            onClick={() => setNav(false)}
+            to="transfer"
+            className="cursor-pointer"
+          >
+            Transfer
+          </Link>
+        </li>
+
+        <div className="flex flex-col my-4">
+          <button className="px-8 py-3 mb-4 text-slate-600 bg-transparent">
+            Sign In
+          </button>
+          <button className="px-8 py-3">Sign Up</button>
+        </div>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
