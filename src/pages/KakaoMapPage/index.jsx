0991f8f1d3d6bd3150cc670274095a20b6693857
Sang Yeong Lee <Sangyeong.lee353@gmail.com>
2023-10-05 08:30:02 +0900

    Feat: update presentation of address info

--- src/pages/KakaoMapPage/index.jsx
+++ src/pages/KakaoMapPage/index.jsx
@@ -3,15 +3,18 @@
  * 주요 기능
  * [구현]
  * 카카오 API를 이용하여 지도 출력
- * 중심 위도와 경도 표시
- * 이동이 완료된 이후 표시
+ * 중심 위도와 경도 표시하기
+ * 드래그 이동 후 마커 표시하기
  * 현재 위치의 마커 표시하기
+ * 클릭한 위치의 마커 표시하기
  * [구현전]
- * 선택한 위치로 이동하기
+ * 마커 초기화 -> Refresh
+ * 주소 검색 + 이동 기능
  * [버전 UP]
  * react-kakako-map-sdk 사용
  * 참고 사항
  * index.html에서 kakao api를 호출할 때는 상관 없으나, 해당 파일에서 kakao api호출 시에는 autoload=false 옵션을 추가해주어야 한다.
+ * 🎇 Function, 🚀 Running function, 💫 Event, 📌 Define
  */
 import { useEffect, useState } from "react";
 import kakaoImg from "../../assets/images/kakao_bg.png";
@@ -38,34 +41,123 @@ const KakaoMapPage = () => {
           center: new window.kakao.maps.LatLng(37.5665, 126.978), // 좌표 설정
           level: 3, // 확대 수준 설정
         };
+        // 💫 Create Map
         const map = new window.kakao.maps.Map(container, options);
 
-        // 🚀 Present marker function in the map
+        // 💫 Create Address to Coordinates object
+        // [Condition]
+        // Must need to property 'libraries=services' in kakao api url.
+        let geocoder = new window.kakao.maps.services.Geocoder();
+
+        // 📌 Define marker & infowindow
+        let marker = new window.kakao.maps.Marker();
+        let infowindow = new window.kakao.maps.InfoWindow({ zindex: 1 });
+
+        // 💫 Present address at map left-top, after search current center of map coordinates.
+        window.kakao.maps.event.addListener(map, "click", (mouseEvent) => {
+          searchDetailAddrFromCoords(mouseEvent.latLng, (result, status) => {
+            if (status === window.kakao.maps.services.Status.OK) {
+              let detailAddr = !!result[0].road_address
+                ? "<div>도로명주소: " +
+                  result[0].road_address.address_name +
+                  "</div>"
+                : "";
+              detailAddr +=
+                "<div>지번 주소: " + result[0].address.address_name + "</div>";
+
+              let content =
+                '<div style="padding: 5px; text-overflow: ellipsis; overflow: hidden; white-space: nowrap;">' +
+                '<span style="display: block; font-weight: bold;">법정동 주소정보</span>' +
+                detailAddr +
+                "</div>";
+
+              // 🚀 Dynamic present marker in the map
+              presentMarker(
+                mouseEvent.latLng.getLat(),
+                mouseEvent.latLng.getLng()
+              );
+
+              // 💫 Save latitude and longitude in the state
+              setLatitude(mouseEvent.latLng.getLat());
+              setLongitude(mouseEvent.latLng.getLng());
+
+              // 💫 Present address info
+              infowindow.setContent(content);
+              infowindow.open(map, marker); // 수정 필요[presentMarker()]
+            }
+          });
+        });
+
+        // 💫 Present address info when changed cetnter position & zoom size
+        window.kakao.maps.event.addListener(map, "idle", () => {
+          searchAddrFromCoords(map.getCenter(), displayCenterInfo);
+        });
+
+        // 🎇 Present marker function in the map
         const presentMarker = (latitude, longitude) => {
           // 1. set marker position
           let position = new window.kakao.maps.LatLng(latitude, longitude);
           // 2. create a marker
-          let marker = new window.kakao.maps.Marker({
-            position: position,
-          });
+          //   let marker = new window.kakao.maps.Marker({
+          //     position: position,
+          //   });
+          // 2. set position of marker
+          marker.setPosition(position);
+
           // 3. create a maker in the map
           marker.setMap(map);
         };
 
-        // 🚀 Static present the map maker
+        // 🎇 행정동 주소 요청 함수
+        const searchAddrFromCoords = (coords, callback) => {
+          geocoder.coord2RegionCode(coords.getLng(), coords.getLat(), callback);
+        };
+
+        // 🎇 법정동 주소 요청 함수
+        const searchDetailAddrFromCoords = (coords, callback) => {
+          geocoder.coord2Address(coords.getLng(), coords.getLat(), callback);
+        };
+
+        // 🎇 지도 좌측 상단에 지도 중심 좌표에 대한 주소 정보 출력 함수
+        const displayCenterInfo = (result, status) => {
+          if (status === window.kakao.maps.services.Status.OK) {
+            let infoDiv = document.getElementById("centerAddr");
+
+            for (let i = 0; i < result.length; i++) {
+              // 행정동의 region_type = 'H'
+              if (result[i].region_type === "H") {
+                infoDiv.innerHTML = result[i].address_name;
+                break;
+              }
+            }
+          }
+        };
+
+        // 🚀 Static present marker in the map
         presentMarker(37.5665, 126.978);
 
-        // 🚀 Get map coordinate info
+        // 💫 Get map coordinate info
         // [1] center_changed: 중심 좌표가 변경된 경우
         // [2] dragend: 드래그가 끝난 경우
-        window.kakao.maps.event.addListener(map, "dragend", () => {
-          let center = map.getCenter();
-          setLatitude(center.getLat());
-          setLongitude(center.getLng());
+        // window.kakao.maps.event.addListener(map, "dragend", () => {
+        //   let center = map.getCenter();
+        //   setLatitude(center.getLat());
+        //   setLongitude(center.getLng());
 
-          // 🚀 Dynamic present the map maker
-          presentMarker(center.getLat(), center.getLng());
-        });
+        //   // 🚀 Dynamic present marker in the map
+        //   presentMarker(center.getLat(), center.getLng());
+        // });
+
+        // [3] click: 마우스를 클릭한 경우
+        // window.kakao.maps.event.addListener(map, "click", (mouseEvent) => {
+        //   // Get & save Clicked latitude and longitude
+        //   let latlng = mouseEvent.latLng;
+        //   setLatitude(latlng.getLat());
+        //   setLongitude(latlng.getLng());
+
+        //   // 🚀 Dynamic present marker in the map
+        //   presentMarker(latlng.getLat(), latlng.getLng());
+        // });
       });
       //   };
 
@@ -96,15 +188,21 @@ const KakaoMapPage = () => {
 
       {/* 콘텐츠 */}
       <Script
-        url={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${KAKAO_API_KEY}&autoload=false`}
+        url={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${KAKAO_API_KEY}&autoload=false&libraries=services`}
         onLoad={getKakaoMapData}
       />
       <div className="flex flex-col absolute w-full h-full">
-        <div className="flex flex-col w-[500px] h-[500px] m-auto z-10">
-          <h2 className="text-center">
+        <div className="flex flex-col w-[500px] h-[500px] m-auto relative">
+          <span className="absolute left-2 z-[3] font-bold text-center">
             위도: {latitude}, 경도: {longitude}
-          </h2>
-          <div id="kakao-map" className="w-full h-full"></div>
+          </span>
+          <div id="kakao-map" className="w-full h-full z-[1]"></div>
+          <div className="absolute left-5 top-5 border rounded-[2px] bg-white bg-opacity-80 z-[2] p-5">
+            <span className="block font-bold">
+              지도중심기준 행정동 주소정보
+            </span>
+            <span id="centerAddr"></span>
+          </div>
         </div>
       </div>
     </section>
